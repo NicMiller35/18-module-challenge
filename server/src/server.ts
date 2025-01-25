153e3fb95575a148ea0022fae65c44e3f6486d7f
Nic Nic Miller <nicmiller13@gmail.com>
2025-01-25 12:21:19 -0500

    debug

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  await db();
 
   const PORT = process.env.PORT || 3001;
   const app = express();
